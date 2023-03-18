3a008fb33c5b0fad116913864cb52eda3ed2104b
systemshift <42102034+systemshift@users.noreply.github.com>
2023-03-18 00:39:18 -0700

    send a stream using single array in call function

--- client/client.go
+++ client/client.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Connect(address, message string) {
+func Connect(address string, message []string) {
 	source, err := net.ResolveUDPAddr("udp", address)
 	conn, err := net.DialUDP("udp", nil, source)
 
@@ -18,10 +18,14 @@ func Connect(address, message string) {
 	fmt.Printf("udp server is %s\n", conn.RemoteAddr().String())
 
 	ticker := time.NewTicker(time.Millisecond * 10)
+	i := 0
 	for range ticker.C {
-		_, err := conn.Write([]byte(message))
+		_, err := conn.Write([]byte(message[i]))
 		if err != nil {
 			log.Default().Println("error writing to udp server")
+		} else {
+			log.Default().Println("message sent to udp server")
+			i += 1
 		}
 		time.Sleep(time.Millisecond * 80)
 
